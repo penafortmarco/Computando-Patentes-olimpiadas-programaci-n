fc7a35f0f0bf1c2d33f69d8d3fb694a3eeef7f08
penafortmarco <penafortmarco@gmail.com>
2023-01-15 18:49:45 -0300

    Added a docstring to all functions in the code that explain how it works

--- new_patent.py
+++ new_patent.py
@@ -1,6 +1,10 @@
 from common import *
 
 def get_new_patent_format(patent, k):
+    """Convert patent in 3 different lists. 2 for letters and one for number. Calls the function get_new_patent_letters 
+    get the new letters and a increment for the number. Calls function get_patent_numbers, return the final patent number. 
+    Then calls again function get_new_patent_letters if is necessary. Finally return a str with the full patent"""
+
     try:
         first_letters = list(patent[:2])
         number = int(patent[2:5])
@@ -8,25 +12,30 @@ def get_new_patent_format(patent, k):
     except (ValueError):
             error_message = 'Not a valid patent'
             return error_message
-            
+
     (result_last_letters, number_increment) = get_new_patent_letters(last_letters, k, False)
     (result_number, letter_increment) = get_patent_numbers(number + number_increment)
     result_first_letters = get_new_patent_letters(first_letters, letter_increment, True)
+    
     result_number = number_to_str(result_number)
 
     return (result_first_letters + result_number + result_last_letters)
 
 def get_new_patent_letters(letters, increment, is_first):
+    """Take the index (int) of each letter in the list according to alphabet. Then call the function get_letter_and_increment.
+    It will return the new letter and an increment for the next letter if it is necessary. If the is_first argument is true and
+    last increment (increment_number_0) is greater than 0 it means that there is no patent "k" positions after the original patent.
+    Return a list with the new_letters and the increment if it's necessary."""
 
     index_1 = alphabet.index(letters[1])
     index_0 = alphabet.index(letters[0])
 
     (letter_1, increment_number_1) = get_letter_and_increment(index_1 + increment, alphabet_size)
     (letter_0, increment_number_0) = get_letter_and_increment(index_0 + increment_number_1, alphabet_size)
+
     if is_first:
         if increment_number_0 > 0:
-            if increment_number_0 > 0:
-             raise Exception('Patent does not exist')
+            raise Exception('Patent does not exist')
         else:
             return ''.join(list(letter_0 + letter_1))
     else:
--- old_patent.py
+++ old_patent.py
@@ -1,7 +1,10 @@
 from common import *
 
 def get_old_patent_format(patent, k):
-    """"""
+    """Convert patent in 2 different lists for number and for letters. Calls the function get_patent_numbers that return 
+    in a tuple result_number that is the final number in the patent and increment_letter, that is the number of letters it 
+    has to be added. Calls the function get_old_pattent_letters if is necessary. Finally return a str with the full patent"""
+
     try:
         letters = list(patent[:3])
         number = int(patent[3:])
@@ -20,7 +23,10 @@ def get_old_patent_format(patent, k):
     return (''.join(letters) + new_number)
 
 def get_old_patent_letters(letters, increment):
-    """"""
+    """Take the index (int) of each letter in the list according to alphabet. Then call the function get_letter_and_increment
+    to calculate every letter. It will return the new letter and an increment for the next letter if it is necessary. If the 
+    last increment (increment_number_0) is greater than 0 it means that there is no patent "k" positions after the original patent.
+    Return a list with the new_letters"""
 
     index_2 = alphabet.index(letters[2])
     index_1 = alphabet.index(letters[1])
@@ -33,4 +39,4 @@ def get_old_patent_letters(letters, increment):
     if increment_number_0 > 0:
         raise Exception('Patent does not exist')
 
-    return str(letter_0) + str(letter_1) + str(letter_2)
+    return letter_0 + letter_1 + letter_2
